e212985039ba284aee576c16dbd85ff02961b4b7
hamidyektaee <hamid.yektaee.social@gmail.com>
2024-10-29 13:16:58 +0330

    Add files via upload

---
+++ src/main/java/azki/com/yektaee/YektaeeApplication.java
@@ -0,0 +1,13 @@
+package azki.com.yektaee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YektaeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YektaeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/azki/com/yektaee/YektaeeApplicationTests.java
@@ -0,0 +1,13 @@
+package azki.com.yektaee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YektaeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
